8da1083bffd0ef8a0de4563bf4697109f28b2d51
Melanie <leblond.melanie@gmail.com>
2026-02-27 16:13:26 +0100

    interfaces objets (clients, produits, ...)

---
+++ src/services/parseCSV.services.ts
@@ -0,0 +1,27 @@
+import * as path from "path";
+import * as fs from "fs"
+
+
+export function parseCSV(file: string): {} {
+    const base = process.cwd();
+    const filePath = path.join(base, "..", '../', "legacy",  'data', file)
+    const data = fs.readFileSync(filePath, 'utf-8').trim()
+    const data_title = file.split(".")[0]
+
+
+    const lines = data.split(/\r?\n/).filter(l => l.trim());
+    const [headerLine, ...dataLines ]= lines;
+
+    const headers = headerLine.split(",")
+
+
+    const result = dataLines.map(line => {
+        const parts = line.split(",");
+
+        return Object.fromEntries(headers.map((header, i) => [header, parts[i]]))
+    })
+
+    return result
+
+}
+
